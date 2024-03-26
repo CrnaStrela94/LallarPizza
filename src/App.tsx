39a658f26b182ec28ea0bf0c0087755cc36fecdc
MikeWaser <mik3waser@gmail.com>
2024-03-26 10:55:25 +0100

    merge from main

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import Header from "./components/Header/Header";
-import MinusBtn from "./components/btn/MinusBtn"
-import OrderBtn from "./components/btn/OrderBtn"
-import { PlusBtn } from "./components/btn/PlusBtn"
+import MinusBtn from "./components/btn/MinusBtn";
+import OrderBtn from "./components/btn/OrderBtn";
+import { PlusBtn } from "./components/btn/PlusBtn";
 export function App() {
   return (
     <>
-    <div>App</div>
-    <Header/>
-    <OrderBtn></OrderBtn>
-    <PlusBtn/>
-    <MinusBtn/>
+      <Header />
+      <div>App</div>
+      <OrderBtn></OrderBtn>
+      <PlusBtn />
+      <MinusBtn />
     </>
-  )
+  );
 }
