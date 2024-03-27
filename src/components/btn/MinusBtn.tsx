4a2de3caa9273deab09b18fde77669936e86210a
EllieBach <elisabeth.bach95@hotmail.com>
2024-03-27 10:37:44 +0100

    nya knappisar

--- src/components/btn/MinusBtn.tsx
+++ src/components/btn/MinusBtn.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import "./__minusbtn.scss"
+import minus from "../../assets/Minus.svg"
+
 const minusClick=()=>{
     
 }
 const MinusBtn = () => {
   return (
     <>
-    <button className="minus-button" onClick={minusClick}><span id='minus'>—</span></button>
+    <button className="minus-button" onClick={minusClick}><img id="minus" src={minus} alt="minus" /></button>
     </>
   )
 }
