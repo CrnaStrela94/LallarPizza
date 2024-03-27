2ac8c122f1915a4d01e9d22d28919a5bc10aca90
MikeWaser <mik3waser@gmail.com>
2024-03-27 10:49:16 +0100

    added Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import Header from "./components/containers/Header/Header";
 import PizzaContainer from "./components/containers/pizzaContainer/PizzaContainer";
 export function App() {
   return (
     <div>
+      <Header />
       <PizzaContainer />
     </div>
   );
