79bbcbfc7f8d16ec54be8646ac84f0e47f8ffa34
EllieBach <elisbeth.bach95@hotmail.com>
2024-04-02 15:24:28 +0200

    css lite grönt på header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <div className="navbar">
             <img src="../src/assets/Lallar_pizza.png" alt="Logo" className="Pizzalogo"/>
-            <h1 className="Title">LallarPizza</h1>
+            <h1 className="Title">La<span className="green">ll</span>arPi<span className="green">zz</span>a</h1>
         </div>
     );
 };
